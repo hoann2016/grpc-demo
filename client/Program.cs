1b1fb0d562595ac510ecba09a55b24830fd5a933
Hoa Nguyen <hoann.2016@gmail.com>
2024-05-23 16:46:30 +0700

    Bi-direction stream

--- client/Program.cs
+++ client/Program.cs
@@ -6,7 +6,7 @@ using Grpc.Core;
 
 const string target="127.0.0.1:50051";
 Channel channel = new Channel(target, ChannelCredentials.Insecure);
-channel.ConnectAsync().ContinueWith((task) =>
+await channel.ConnectAsync().ContinueWith((task) =>
 {
     if (task.Status == TaskStatus.RanToCompletion)
     {
