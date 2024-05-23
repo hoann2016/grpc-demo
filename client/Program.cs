84b4554046846b02bfcc50d7f8e8b7abcab8f8e1
Hoa Nguyen <hoann.2016@gmail.com>
2024-05-23 13:10:24 +0700

    init

---
+++ client/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Dumy;
+using Grpc.Core;
+
+const string target="127.0.0.1:50051";
+Channel channel = new Channel(target, ChannelCredentials.Insecure);
+channel.ConnectAsync().ContinueWith((task) =>
+{
+    if (task.Status == TaskStatus.RanToCompletion)
+    {
+        Console.WriteLine("The client connected successfully");
+    }
+});
+var client=new DummyService.DummyServiceClient(channel);
+channel.ShutdownAsync().Wait();
+Console.Read();
---
+++ server/Program.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+
+const int port =50051;
+Server server=null;
+try
+{
+    server = new Server
+    {
+       
+        Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+    };
+    server.Start();
+    Console.WriteLine("Server listening on port " + port);
+    Console.WriteLine("Press any key to stop the server...");
+    Console.Read();
+}
+catch (Exception e)
+{
+    Console.WriteLine("An error occurred: " + e.Message);
+}
+finally
+{
+    
+    if (server != null)
+    {
+        server.ShutdownAsync().Wait();
+    }
+}
